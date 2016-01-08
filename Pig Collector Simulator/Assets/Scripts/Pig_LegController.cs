25fbf23c3f452bf6f6d414070c826d9e6e18c64c
Jared Ramey <rameyjared@gmail.com>
2016-01-07 17:37:51 -0800

    Finished my day of work

--- Pig Collector Simulator/Assets/Scripts/Pig_LegController.cs
+++ Pig Collector Simulator/Assets/Scripts/Pig_LegController.cs
@@ -4,16 +4,20 @@ using System.Collections;
 public class Pig_LegController : MonoBehaviour
 {
 
+    private GameObject PigBody;
+    public Vector3 PigBodyVel;
+
     // Use this for initialization
     void Start()
     {
-
+        //Get the body of the pig
+        PigBody = transform.parent.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        PigBodyVel = PigBody.GetComponent<Rigidbody>().velocity;
     }
 
     /*
@@ -21,6 +25,10 @@ public class Pig_LegController : MonoBehaviour
     */
     void OnCollisionEnter(Collision collision)
     {
-
+        /*
+         * TODO:
+         * Need to figure out a way to make the pig bounce when the legs hit the ground
+        */
+        PigBody.GetComponent<Rigidbody>().velocity = -PigBody.GetComponent<Rigidbody>().velocity;
     }
 }
