edb957d8adc621ae60286922caf82f417413c9c3
Jared Ramey <rameyjared@gmail.com>
2016-01-08 10:42:03 -0800

    Pig work
    Pig can now overcome the y force that was causing the pig to always move
    in one direction. Pig controller on each leg must have a max velocity of
    at least 10 to achieve the desired effect as far as I can tell as of
    yet.

--- Pig Collector Simulator/Assets/Scripts/Pig_LegController.cs
+++ Pig Collector Simulator/Assets/Scripts/Pig_LegController.cs
@@ -5,19 +5,22 @@ public class Pig_LegController : MonoBehaviour
 {
 
     private GameObject PigBody;
-    public Vector3 PigBodyVel;
+    //Set maximum velocity possible to add
+    public int randVel;
 
     // Use this for initialization
     void Start()
     {
         //Get the body of the pig
-        PigBody = transform.parent.gameObject;
+        PigBody = transform.parent.parent.parent.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        PigBodyVel = PigBody.GetComponent<Rigidbody>().velocity;
+        /*
+         * If anything ends up needing to be updated
+        */
     }
 
     /*
@@ -27,8 +30,58 @@ public class Pig_LegController : MonoBehaviour
     {
         /*
          * TODO:
-         * Need to figure out a way to make the pig bounce when the legs hit the ground
+         * Add a random velocity in a random direction 
         */
-        PigBody.GetComponent<Rigidbody>().velocity = -PigBody.GetComponent<Rigidbody>().velocity;
+        //PigBody.GetComponent<Rigidbody>().velocity = -PigBody.GetComponent<Rigidbody>().velocity;
+        //Get a random direction
+        int randDir = Random.Range(0, 4);
+        switch(randDir)
+        {
+            //Back
+            case 0:
+                //Print out to make sure this is working
+                print("back");
+                PigBody.GetComponent<Rigidbody>().velocity += Vector3.back * Random.Range(0, randVel);
+                break;
+            //Forward
+            case 1:
+                print("forward");
+                PigBody.GetComponent<Rigidbody>().velocity += Vector3.forward * Random.Range(0, randVel);
+                break;
+            //Left
+            case 2:
+                print("left");
+                PigBody.GetComponent<Rigidbody>().velocity += Vector3.left * Random.Range(0, randVel);
+                break;
+            //Right
+            case 3:
+                print("right");
+                PigBody.GetComponent<Rigidbody>().velocity += Vector3.right * Random.Range(0, randVel);
+                break;
+            //Up
+            case 4:
+                print("up");
+                PigBody.GetComponent<Rigidbody>().velocity += Vector3.up * Random.Range(0, randVel);
+                break;
+
+            //If something happens to out of scope my rand
+            //default will just keep the pig going
+            default:
+                print("default");
+                break;
+        }
+        
+    }
+
+    /*Quick little randomizer funtion that can be coppied over to any other script*/
+    float Randomizer(int seed)
+    {
+        //Random number to return
+        float rand;
+
+        //Set the seed
+        Random.seed = seed;
+
+        return rand = Random.value;
     }
 }
