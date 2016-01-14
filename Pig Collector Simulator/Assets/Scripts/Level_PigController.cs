6f6f5fd658462e55dd0d69e4424d0bfe45a1a9e2
Jared Ramey <rameyjared@gmail.com>
2016-01-13 17:55:36 -0800

    Pen and Controller Work
    Pen now captures pigs and disables them.
    Next step is to get the level controller to register all pigs have been
    captured

--- Pig Collector Simulator/Assets/Scripts/Level_PigController.cs
+++ Pig Collector Simulator/Assets/Scripts/Level_PigController.cs
@@ -11,16 +11,42 @@ public class Level_PigController : MonoBehaviour
     //All variables needed
     public GameObject[] pigs;
     public GameObject corrall;
+    public GameObject pigPen_Controller;
 
     // Use this for initialization
     void Start()
     {
         pigs = GameObject.FindGameObjectsWithTag("Pig");
         print("" + pigs.Length);
+
+        pigPen_Controller = GameObject.Find("Level_PigContoller");
     }
 
     // Update is called once per frame
     void Update()
+    {
+        /*
+         * TODO:
+         * Check to see if all pigs are in the pen
+         * If all pigs are in the pen then activate "The Ending"
+         * Put player back to main menu (if we have one)
+        */
+    }
+
+    void CheckPen()
+    {
+        //Check to see how many pigs are in the pen
+        PigPen_Controller controller = pigPen_Controller.GetComponent<PigPen_Controller>();
+
+        int ActualPigs_InPen = controller.pigsInPen / 4;
+
+        if(ActualPigs_InPen == pigs.Length)
+        {
+            print("All pigs have been collected");
+        }
+    }
+
+    void TheEnd()
     {
 
     }
--- Pig Collector Simulator/Assets/Scripts/PigPen_Controller.cs
+++ Pig Collector Simulator/Assets/Scripts/PigPen_Controller.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class PigPen_Controller : MonoBehaviour
 {
+    public int pigsInPen = 0;
 
     // Use this for initialization
     void Start()
@@ -10,12 +11,57 @@ public class PigPen_Controller : MonoBehaviour
 
     }
 
+    void Update()
+    {
+        
+    }
+
     void OnTriggerEnter(Collider collision)
     {
-        print("Collision w/ pen!");
+        //print("Collision w/ pen!");
         if (collision.gameObject.tag.ToString() == "Pig")
         {
+
+            pigsInPen++;
             collision.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
+            Destroy(collision.gameObject.GetComponent<Pig_Controller>());
+            
+            //Destroy(collision.gameObject.GetComponent<Pig_LegController>());
+
+            /*
+             * Have to drig through each layer to check every object
+            */
+            foreach (Transform child in collision.gameObject.transform)
+            {
+                /*
+                 * Have to drig through each layer to check every object
+                */
+                print(child.gameObject.name);
+                if (child.gameObject.GetComponent<Pig_Controller>())
+                {
+                    //print("found a body controller script");
+                    Destroy(child.gameObject.GetComponent<Pig_Controller>());
+                }
+
+                foreach (Transform subChild in child.gameObject.transform)
+                {
+                    /*
+                     * Have to drig through each layer to check every object
+                    */
+                    print(subChild.gameObject.name);
+
+                    foreach (Transform subSubChild in subChild.gameObject.transform)
+                    {
+                        print(subSubChild.gameObject.name);
+                        if(subSubChild.gameObject.GetComponent<Pig_LegController>())
+                        {
+                            //print("found a leg controller script");
+                            Destroy(subSubChild.gameObject.GetComponent<Pig_LegController>());
+                            pigsInPen++;
+                        }
+                    }
+                }
+            }
         }
     }
 }
