77810bead456b8eb4ae0a4df1c438fbc0c6b35cb
Jared Ramey <rameyjared@gmail.com>
2016-01-15 17:44:25 -0800

    Started the end times
    Next week I need to add blood particles where the hinge joints were
    destroyed

--- Pig Collector Simulator/Assets/Scripts/Level_PigController.cs
+++ Pig Collector Simulator/Assets/Scripts/Level_PigController.cs
@@ -18,6 +18,8 @@ public class Level_PigController : MonoBehaviour
     public GameObject minSpawnPos, maxSpawnPos;
     public Text pigsRemaning_UI;
     public Text WinningText;
+    public float randVel;
+    public float delay;
 
 
     //Private variables
@@ -93,11 +95,122 @@ public class Level_PigController : MonoBehaviour
         if(numPigsRemaining <= 0)
         {
             WinningText.text = "You Win!";
+            TheEnd();
         }
     }
 
     void TheEnd()
     {
-        
+        /*
+         * TODO:
+         * - Add a random velocity to each pig
+         * - Destroy each pig limb
+         * - Add particle effect to each limb
+        */
+
+        RandomVel();
+        DestroyLimbs();
     }
+
+    void RandomVel()
+    {
+        /*
+         * TODO:
+         * Add a random velocity in a random direction 
+        */
+        //PigBody.GetComponent<Rigidbody>().velocity = -PigBody.GetComponent<Rigidbody>().velocity;
+        //Get a random direction
+        foreach (GameObject pig in pigs)
+        {
+            int randDir = Random.Range(0, 4);
+            float actRandVel = Random.Range(0, randVel);
+            switch (randDir)
+            {
+                //Back
+                case 0:
+                    //Print out to make sure this is working
+                    //print("back");
+                    pig.GetComponent<Rigidbody>().velocity += Vector3.back * Random.Range(0, actRandVel);
+                    break;
+                //Forward
+                case 1:
+                    // print("forward");
+                    pig.GetComponent<Rigidbody>().velocity += Vector3.forward * Random.Range(0, actRandVel);
+                    break;
+                //Left
+                case 2:
+                    //print("left");
+                    pig.GetComponent<Rigidbody>().velocity += Vector3.left * Random.Range(0, actRandVel);
+                    break;
+                //Right
+                case 3:
+                    //print("right");
+                    pig.GetComponent<Rigidbody>().velocity += Vector3.right * Random.Range(0, actRandVel);
+                    break;
+                //Up
+                case 4:
+                    //print("up");
+                    pig.GetComponent<Rigidbody>().velocity += Vector3.up * Random.Range(0, actRandVel);
+                    break;
+
+                //If something happens to out of scope my rand
+                //default will just keep the pig going
+                default:
+                    //print("default");
+                    break;
+            }
+        }
+
+    }
+
+    void DestroyLimbs()
+    {
+        foreach(GameObject pig in pigs)
+        {
+            // Emtpy Game Object Wrapper
+            Destroy(pig.GetComponent<HingeJoint>(), delay);
+
+            /*
+             * Have to drig through each layer to check every object
+            */
+            foreach (Transform child in pig.transform)
+            {
+                /*
+                 * Have to drig through each layer to check every object
+                */
+                //Body of pig
+                if (child.gameObject.GetComponent<HingeJoint>())
+                {
+                    Destroy(child.gameObject.GetComponent<HingeJoint>(), delay);
+                }
+
+                foreach (Transform subChild in child.gameObject.transform)
+                {
+                    /*
+                     * Have to drig through each layer to check every object
+                    */
+                    //Legs back/ legs front
+                    foreach (Transform subSubChild in subChild.gameObject.transform)
+                    {
+                        print(subSubChild.gameObject.name);
+                        if (subSubChild.gameObject.GetComponent<HingeJoint>())
+                        {
+                            Destroy(subSubChild.gameObject.GetComponent<HingeJoint>(), delay);
+                        }
+                    }
+                }
+            }
+        }
+
+        //After everything is getting ready to be destroyed
+        //Let the blood fly
+        BloodyTime();
+    }
+
+    void BloodyTime()
+    {
+
+    }
+
+
 }
